1c9cd0fc1c2cbd62fa1909d6ab5a61378aa59a78
Bao Trong <nhbaotrong@gmail.com>
2025-10-19 15:33:49 +0700

    chore: change layout of public profile & privite profile

--- src/app/(user-dashboard)/profile/page.tsx
+++ src/app/(user-dashboard)/profile/page.tsx
@@ -4,16 +4,24 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Button } from '@/components/ui/button'
 import { Card } from '@/components/ui/card'
 import { useParams } from 'next/navigation'
-import ProfileContent from '../components/_profile-content/profile-content'
-import About from '../components/about'
-import SuggestionList from '../components/suggestion-list'
+import ProfileContent from './components/_profile-content/profile-content'
+import About from './components/about'
+import SuggestionList from './components/suggestion-list'
+import { authHelpers } from '@/lib/supabase'
+import { useUserStore } from '@/stores/user-store'
 
 export default function ProfilePage() {
      const isOwnProfile = false
-     const { id: userId } = useParams()
+
+     const { user, fetchUser } = useUserStore()
+
+     const handleRefresh = () => {
+          fetchUser()
+     }
 
      return (
           <div className='container mx-auto p-5'>
+               <Button onClick={handleRefresh}>Test</Button>
                <Card className=''>
                     <div className='relative'>
                          <div className='relative group'>
@@ -21,10 +29,10 @@ export default function ProfilePage() {
                          </div>
                          <div className='flex items-end'>
                               <Avatar className='w-48 h-48'>
-                                   <AvatarImage src="https://github.com/shadcn.png" />
+                                   <AvatarImage src={user?.user_metadata.picture} />
                                    <AvatarFallback>CN</AvatarFallback>
                               </Avatar>
-                              <span className='text-3xl font-bold mb-5 ml-3'>Trà Thảo</span>
+                              <span className='text-3xl font-bold mb-5 ml-3'>{user?.user_metadata.name}</span>
                          </div>
                     </div>
                     <div className='h-30 flex justify-end items-center gap-5 px-20'>
