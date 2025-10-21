ae33a2d544ff4415662c0787c49c8931cf69ad9b
Bao Trong <nhbaotrong@gmail.com>
2025-10-22 00:06:13 +0700

    feat: post detail page

--- src/app/(public-layout)/forum/[id]/page.tsx
+++ src/app/(public-layout)/forum/[id]/page.tsx
@@ -1,15 +1,23 @@
 'use client'
 
-import React from 'react'
-import { useParams } from 'next/navigation'
+import Comment from '@/components/forum/post/comment'
+import Post from '@/components/forum/post/post'
+import { Card } from '@/components/ui/card'
 import { usePostDetail } from '@/lib/hooks/usePostHooks'
+import { useParams } from 'next/navigation'
 
 export default function PostDetail() {
      const { id } = useParams()
      const { data } = usePostDetail(id as string)
-     console.log(data);
-     
+
      return (
-          <h1>{id}</h1>
+          <div className='container md:px-40'>
+               <Card className='p-10 pt-0 mt-5'>
+                    <Post dataPost={data?.data} />
+                    <h1 className='scroll-m-20 border-b pb-2 text-xl font-semibold tracking-tight first:mt-0'>Comment</h1>
+                    <Comment hasChild />
+                    <Comment />
+               </Card>
+          </div>
      )
 }
