576facd534f34cd152fe35fc2a6e755dc7680a5d
Aries <lehuy25032004@gmail.com>
2025-11-03 21:21:54 +0700

    chore(marketplace): updated skeleton

--- src/components/marketplace/marketplace-skeleton.tsx
+++ src/components/marketplace/marketplace-skeleton.tsx
@@ -5,7 +5,15 @@ export default function MarketplaceSkeleton() {
     return (
         <div className="w-full">
             <div className='flex flex-col gap-8'>
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+                <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-6">
+                    <div className='flex flex-col gap-2'>
+                        <Skeleton className="w-full aspect-[3/2] rounded-xl mt-2" />
+                        <div className='flex justify-start items-center gap-2'>
+                            <Skeleton className="h-10 w-10 rounded-full" />
+                            <Skeleton className="h-4 w-1/3" />
+                            <Skeleton className="h-4 w-2/3" />
+                        </div>
+                    </div>
                     <div className='flex flex-col gap-2'>
                         <Skeleton className="w-full aspect-[3/2] rounded-xl mt-2" />
                         <div className='flex justify-start items-center gap-2'>
