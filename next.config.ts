9af24e3b401c122fa41af991f3c004665ae2d24f
Hoang Kim Long <longhk.dev@gmail.com>
2025-11-21 02:50:09 +0700

    chore: add backend server hostname to next.config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 import { BASE_URL } from "@/constants";
 import type { NextConfig } from "next";
-import createNextIntlPlugin from 'next-intl/plugin';
+import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['images.unsplash.com', 'i.pinimg.com', '14.169.52.232'],
-  }
+    domains: ["images.unsplash.com", "i.pinimg.com", "14.169.52.232", "lensor.klong.io.vn"],
+  },
 };
 const withNextIntl = createNextIntlPlugin();
 
